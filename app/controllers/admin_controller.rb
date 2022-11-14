baeb3a1b9668a685c5ffef28fb73445f07cb9f2d
MeganJade5 <98874054+MeganJade5@users.noreply.github.com>
2022-11-15 10:34:11 +1100

    admin updates

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,10 +1,14 @@
 class AdminController < ApplicationController
-  before_action :is_admin?
+  before_action :set_post, only: [:show, :update, :destroy]
+  before_action :authorize, only: [:show, :update, :destroy]
 
   def index
+    posts = Post.all.includes(:user, :cuisine, :food_prep)
+    render json: posts, include: {cuisine: {only: :name}, food_prep: {only: :name}, user: {only: :email}}, status: 200
   end
 
   def posts
+    render json: @post, include: {cuisine: {only: :name}, food_prep: {only: :name}, user: {only: :email}}, status: 200
   end
 
   def show_post
@@ -12,6 +16,16 @@ class AdminController < ApplicationController
 
   private
 
-  def is_admin?
-    redirect_to root_path unless current_user.admin?
+    def authorize
+        render json: {error: "You don't have permission to do that"}, status: 401 unless current_user.admin?
+    end
+
+  def set_post
+      begin
+      @post = Post.find(params[:id])
+      rescue
+          render json: {error: "Unable to find post"}, status: 404
+      end
+  end
+
 end
--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Admins", type: :request do
   describe "GET /index" do
+    before(:all)do
+      @admin = create(:admin)
+    end
+  end
+
+    context "with correct credentials" do
+      before(:each) do
+        post "/auth/signin", params: {auth: {email: 'admin@glutenfreefinder.com', password: 'admin123'}}
+      end
+    end
+
     it "returns http success" do
       get "/admin"
       expect(response).to have_http_status(:success)
     end
-  end
 
   describe "GET /posts" do
     it "returns http success" do
