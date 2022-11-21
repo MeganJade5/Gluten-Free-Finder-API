ae7903464b3514c9439c40fb8dd8b0ef7813b90b
MeganJade5 <98874054+MeganJade5@users.noreply.github.com>
2022-11-21 13:50:22 +1100

    config.active_support.key_generator_hash_digest_class = OpenSSL::Digest::SHA1 to application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module GlutenFreeFinderApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.active_support.key_generator_hash_digest_class = OpenSSL::Digest::SHA1
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
